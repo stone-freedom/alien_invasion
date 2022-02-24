5b60a9366693ec1a495b723bcb10f62bbb697371
stone <yadiraies@gmail.com>
2022-02-24 20:42:48 +0800

    first_ship

---
+++ settings.py
@@ -0,0 +1,10 @@
+
+
+class Settings():
+    """存储所有设置的类"""
+
+    def __init__(self):
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
