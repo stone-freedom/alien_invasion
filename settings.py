36244a99da07261448af66e4fe773d78f7231686
stone <yadiraies@gmail.com>
2022-02-27 17:37:52 +0800

    调整飞船速度

--- settings.py
+++ settings.py
@@ -1,5 +1,3 @@
-
-
 class Settings():
     """存储所有设置的类"""
 
@@ -8,3 +6,5 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        # 飞船的设置
+        self.ship_speed_factor = 1.5
--- ship.py
+++ ship.py
@@ -3,9 +3,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen) -> None:
+    def __init__(self, ai_settings, screen) -> None:
         # 初始化飞船并设置其初始位置
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # 加载飞船图像并获取其外接矩形
         self.image = pygame.image.load('./images/ship.bmp')
@@ -15,6 +16,19 @@ class Ship():
         # 将新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        # 将飞船的属性center中存储小数值
+        self.center = float(self.rect.centerx)
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """根据移动标志调整飞船位置"""
+        if self.moving_right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
 
     def blitime(self):
         # 在指定位置绘制飞船
